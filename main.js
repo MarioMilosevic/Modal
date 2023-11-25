eb4a466cff2eab94a3454764a8868d5fc0ae31bb
Mario Milošević <mariomilosevic887@gmail.com>
2023-11-25 14:55:32 +0100

    Make layout

---
+++ main.js
@@ -0,0 +1,11 @@
+const app = document.querySelector('#app');
+app.innerHTML = `
+<div class="mainDiv">
+  <div class="clickableDivs">
+    <div class="clickable">1</div>
+    <div class="clickable">2</div>
+    <div class="clickable">3</div>
+  </div>
+  <div class="textDiv"></div>
+</div>`
+
